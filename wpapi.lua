6a2f8624de5cb77d327696e22c5e9efac045bbbd
capped-_- <166282207+capped-uwu@users.noreply.github.com>
2024-11-01 16:07:56 +0900

    Create wpapi.lua

---
+++ wpapi.lua
@@ -0,0 +1,10 @@
+print("wpapi loaded :)")
+print("all credit goes to xeno api + forlorn api")
+
+local starterGui = game:GetService("StarterGui")
+
+starterGui:SetCore("SendNotification", {
+	Title = "wpapi loaded!",
+	Text = "wpapi loaded :)",
+	Duration = 25
+})
